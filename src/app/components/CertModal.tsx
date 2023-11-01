5c1f0ed5cd3088d0d7ced8c4ffba184add5aa630
Arxeno <rmfbkw04@gmail.com>
2023-11-01 19:36:07 +0700

    refactoring cert modal

---
+++ src/app/components/CertModal.tsx
@@ -0,0 +1,145 @@
+import { CertDetails, TicketDetails } from "@/type";
+import { copyImageToClipboard } from "copy-image-clipboard";
+
+interface Props {
+  modal: Function;
+  showToast: Function;
+  details: CertDetails | null;
+  image: string;
+  pdf: string;
+}
+
+export default function CertModal({
+  modal,
+  showToast,
+  details,
+  image,
+  pdf,
+}: Props): JSX.Element {
+  return (
+    <div className="fade fixed lg:h-fit h-[85%] flex flex-col items-center sm:gap-[20px] gap-[10px] p-[20px] lg:w-[1000px] md:w-[700px] w-[337px] rounded-[20px] bg-white">
+      <div className="flex flex-row w-full justify-between items-center">
+        <img
+          src={"/icons/cross_box.svg"}
+          className="invisible md:block hidden"
+        ></img>
+        <p className="font-bold md:text-[36px] text-[30px] text-[#5038BC] ">
+          RISTEK Verify
+        </p>
+        <img
+          src={"/icons/cross_box.svg"}
+          onClick={() => modal()}
+          className="cursor-pointer"
+        ></img>
+      </div>
+
+      <div className="flex lg:flex-row flex-col items-center gap-[30px] xl:overflow-hidden overflow-auto h-fit">
+        <div className="flex flex-col md:w-[600px] w-[297px] gap-[10px]">
+          <div className="flex flex-col items-center justify-center bg-[#DDDDDD] gap-[8px] rounded-[20px] md:pl-[32px] pl-[10px] md:pr-[32px] pr-[10px] md:pt-[20px] pt-[10px] md:pb-[20px] pb-[10px]">
+            <div className="flex w-[100%] flex-row items-center bg-white rounded-[8px] gap-[8px] h-[32px] pl-[16px]">
+              <p className="md:text-base text-xs">
+                Certificate ID: <span className="font-bold">{details?.id}</span>
+              </p>
+              <img
+                className="cursor-pointer"
+                onClick={() => {
+                  {
+                    navigator.clipboard.writeText(details ? details.id : ""),
+                      showToast("Certificate ID copied to clipboard!");
+                  }
+                }}
+                src={"/icons/copy-icon.svg"}
+              />
+            </div>
+            {image == "data:," || image == "" ? (
+              "Loading..."
+            ) : (
+              <img
+                className="w-[600px]"
+                src={image}
+              />
+            )}
+          </div>
+          <p className="text-center md:text-[16px] text-xs font-[400px] italic leading-6">
+            This certificate verifies that
+            <span className="font-bold">
+              {" "}
+              {details ? details.recipient : ""}{" "}
+            </span>
+            successfully completed
+            <span className="font-bold">
+              {" "}
+              {details ? details.eventName : ""}{" "}
+            </span>
+            as a
+            <span className="font-bold"> {details ? details.asWhat : ""} </span>
+            .
+          </p>
+        </div>
+
+        <div className="flex lg:flex-col md:flex-row flex-col lg:w-[300px] md:w-[1000px] w-[250px] pb-[40px] md:gap-[20px] gap-[8px] justify-center">
+          <div className="flex flex-col gap-[4px]">
+            <p className="md:text-[24px] text-base font-bold">Detail:</p>
+
+            <div className="flex md:text-[16px] text-xs flex-col gap-[4px] md:leading-6 leading-4">
+              <div>
+                <p>Recipent:</p>
+                <p className="font-bold">{details ? details.recipient : ""}</p>
+              </div>
+
+              <div>
+                <p>Issued by RISTEK for Event:</p>
+                <p className="font-bold">{details ? details.eventName : ""}</p>
+              </div>
+
+              <div>
+                <p>As:</p>
+                <p className="font-bold">{details ? details.asWhat : ""}</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-[18px]">
+            <p className="underline">
+              Issued on: {details ? details.issuedDate : ""}
+            </p>
+
+            <div className="flex flex-col gap-[10px] items-center md:text-base text-[14px] text-center">
+              <p className="text-[18px] font-bold">Download Certificate</p>
+              <div className="flex flex-row gap-[10px]">
+                <a
+                  href={pdf}
+                  download="Certificate.pdf"
+                >
+                  <button className="bg-[#C9CEFC] rounded-[8px] md:h-[48px] h-[28px] md:w-[145px] w-[86.5px] text-[#45349F]">
+                    as PDF
+                  </button>
+                </a>
+
+                <a
+                  href={image}
+                  download="Certificate.png"
+                >
+                  <button className="bg-[#C9CEFC] rounded-[8px] md:h-[48px] h-[28px] md:w-[145px] w-[86.5px] text-[#45349F]">
+                    as PNG
+                  </button>
+                </a>
+              </div>
+              <button
+                onClick={() => {
+                  {
+                    copyImageToClipboard(image),
+                      showToast("Image copied to clipboard!");
+                  }
+                }}
+                className="bg-[#C9CEFC] rounded-[8px] md:h-[48px] h-[32px] md:w-[100%] w-[183px] text-[#45349F]"
+              >
+                Copy Image
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,110 +1,121 @@
-'use client'
+"use client";
 import { useState } from "react";
 import About from "./sections/About";
 import Contributors from "./sections/Contributors";
 import Footer from "./sections/Footer";
 import HeroSection from "./sections/HeroSection";
 import NavigationBar from "./components/NavigationBar";
-import Modal from "./components/Modal";
 import axios from "axios";
-import { ToastContainer, toast, Slide } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';  
+import { ToastContainer, toast, Slide } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import React from "react";
-
-interface Details {
-    cert_id : string,
-    name : string,
-    eventName : string,
-    asWhat : string,
-    issuedDate : string
-}
+import { CertDetails, TicketDetails } from "@/type";
+import CertModal from "./components/CertModal";
 
 export default function Home() {
-    const endpoint = "https://ristek-verify.cloud"
+  const endpoint = "https://ristek-verify.cloud";
 
-    const [modal, showModal] = useState(false);
+  const [modal, showModal] = useState(false);
 
-    const [details, setDetails] = useState<Details>({
-        cert_id : "",
-        name : "",
-        eventName : "",
-        asWhat : "",
-        issuedDate : ""
+  const [certDetails, setCertDetails] = useState<CertDetails | null>(null);
+  const [ticketDetails, setTicketDetails] = useState<TicketDetails | null>(
+    null
+  );
+
+  const [image, setImage] = useState<string>("");
+  const [pdf, setPdf] = useState<string>("");
+
+  const showToast = (message: string) =>
+    toast.info(message, {
+      icon: false,
+      position: "bottom-center",
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
     });
-    
-    const [image, setImage] = useState('');
-    const [pdf, setPdf] = useState('');
 
-    const showToast = (message : string) => toast.info(message, {
-        icon: false,
-        position: "bottom-center",
-        autoClose: 5000,
-        hideProgressBar: true,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-        });
-    
-    const verifyError = () => toast.error('Certificate with the given ID was not found!', {
-        position: "bottom-center",
-        autoClose: 5000,
-        hideProgressBar: true,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        theme: "colored",
-        });
+  const verifyError = () =>
+    toast.error("Certificate with the given ID was not found!", {
+      position: "bottom-center",
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      theme: "colored",
+    });
 
-    const fetchCertificate = async (inp : string) => {
-        showToast('Verifying...')
+  const fetchCertificate = async (inp: string) => {
+    showToast("Verifying...");
 
-        try {
-            const data = await axios.get(`${endpoint}/verify/certification/json/${inp}`)
+    try {
+      const response = await axios.get(`${endpoint}/verify/${inp}`);
+      console.log(response);
 
-            if (data.status == 200){
-                const details = data.data
-                setDetails({cert_id : inp, ... details})
-                setImage(`${endpoint}/verify/certification/image/${inp}`)
-                setPdf(`${endpoint}/verify/certification/pdf/${inp}`)
+      if (response.status == 200) {
+        const details = response.data.data;
+        if (response.data.isCert) {
+          setCertDetails(details);
+          setImage(`${endpoint}/verify/certification/image/${inp}`);
+          setPdf(`${endpoint}/verify/certification/pdf/${inp}`);
 
-                toast.dismiss()
-                showModal(true)
-            }
+          setTicketDetails(null);
+        } else {
+          setTicketDetails(details);
 
-        } catch (error) {
-            toast.dismiss()
-            verifyError()
+          setCertDetails(null);
+          setImage("");
+          setPdf("");
         }
+
+        toast.dismiss();
+        showModal(true);
+      }
+    } catch (error) {
+      toast.dismiss();
+      verifyError();
     }
+  };
 
   return (
-    <div>   
-        <ToastContainer
-            transition={Slide}
-            style={{ width: "450px"}}
-        />
+    <div>
+      <ToastContainer
+        transition={Slide}
+        style={{ width: "450px" }}
+      />
 
-        { modal &&
-            <div className="fixed z-[14] w-[100%] h-[100%] mt-[-32px] flex items-center justify-center ">
-                <div className="fixed w-[100%] h-[100%] bg-black opacity-30" onClick={() => showModal(false)}></div>
-                <Modal modal={() => showModal(false)} 
-                    showToast={showToast} 
-                    details={details} 
-                    image={image} 
-                    pdf={pdf}/>
-            </div>
-        }
+      {modal && (
+        <div className="fixed z-[14] w-[100%] h-[100%] mt-[-32px] flex items-center justify-center ">
+          <div
+            className="fixed w-[100%] h-[100%] bg-black opacity-30"
+            onClick={() => showModal(false)}
+          ></div>
 
-        <div className="bg-[#F1F5F9] mt-[64px]">
-            <NavigationBar/>
-            <HeroSection modal={fetchCertificate}/>
-            <About/>
-            <Contributors/>
-            <Footer />
+          {certDetails ? (
+            <CertModal
+              modal={() => showModal(false)}
+              showToast={showToast}
+              details={certDetails}
+              image={image}
+              pdf={pdf}
+            />
+          ) : (
+            <p className="fixed top-0 left-0">ticket modal</p>
+          )}
         </div>
+      )}
+
+      <div className="bg-[#F1F5F9] mt-[64px]">
+        <NavigationBar />
+        <HeroSection modal={fetchCertificate} />
+        <About />
+        <Contributors />
+        <Footer />
+      </div>
     </div>
   );
 }
-
